2fd0bf43397bfa364db810bc4a483e80aecf3997
antelle <dmitryvit@gmail.com>
2021-05-15 16:13:05 +0200

    model

---
+++ app/scripts/framework/model.ts
@@ -0,0 +1,159 @@
+import { TypedEmitter } from 'tiny-typed-emitter';
+
+type ListenerSignature<EventSpec> = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [EventName in keyof EventSpec]: (...args: any[]) => any;
+};
+
+export interface DefaultModelEvents {
+    'change': () => void;
+}
+
+interface ModelTypedEmitter extends TypedEmitter {
+    changePending?: boolean;
+    batchSet?: boolean;
+    silent?: boolean;
+}
+
+const DefaultMaxListeners = 100;
+
+const SymbolEmitter = Symbol('emitter');
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+function emitPropChange(target: Model, prop: string, value: unknown, prevValue: unknown) {
+    const emitter = target[SymbolEmitter] as ModelTypedEmitter;
+    if (emitter) {
+        if (!emitter.silent) {
+            emitter.emit(`change:${prop}`, value, prevValue);
+            if (emitter.batchSet) {
+                emitter.changePending = true;
+            } else {
+                emitter.emit(`change`);
+            }
+        }
+    }
+}
+
+const ProxyDef: ProxyHandler<any> = {
+    deleteProperty(target: any, prop: string | symbol): boolean {
+        const prevValue = target[prop];
+        const value = undefined;
+        if (prevValue !== value) {
+            delete target[prop];
+            if (typeof prop === 'string') {
+                emitPropChange(target, prop, value, prevValue);
+            }
+        }
+        return true;
+    },
+
+    set(target: any, prop: string | symbol, value: unknown): boolean {
+        const prevValue = target[prop];
+        if (prevValue !== value) {
+            target[prop] = value;
+            if (typeof prop === 'string') {
+                emitPropChange(target, prop, value, prevValue);
+            }
+        }
+        return true;
+    }
+};
+
+/* eslint-enable */
+
+type NonFunctionPropertyNames<T> = {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    [K in keyof T]: T[K] extends Function ? never : K;
+}[keyof T];
+
+export class Model<EventSpec extends ListenerSignature<EventSpec> = DefaultModelEvents> {
+    constructor() {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return new Proxy(this, ProxyDef);
+    }
+
+    [SymbolEmitter]: TypedEmitter<EventSpec>;
+
+    private emitter(): TypedEmitter<EventSpec> {
+        let emitter = this[SymbolEmitter];
+        if (!emitter) {
+            this[SymbolEmitter] = emitter = new TypedEmitter<EventSpec>();
+            emitter.setMaxListeners(DefaultMaxListeners);
+        }
+        return emitter;
+    }
+
+    protected emit<EventName extends keyof EventSpec>(
+        event: EventName,
+        ...args: Parameters<EventSpec[EventName]>
+    ): boolean {
+        return this.emitter().emit(event, ...args);
+    }
+
+    once<EventName extends keyof EventSpec>(
+        event: EventName,
+        listener: EventSpec[EventName]
+    ): this {
+        this.emitter().once(event, listener);
+        return this;
+    }
+
+    on<EventName extends keyof EventSpec>(event: EventName, listener: EventSpec[EventName]): this {
+        this.emitter().on(event, listener);
+        return this;
+    }
+
+    off<EventName extends keyof EventSpec>(event: EventName, listener: EventSpec[EventName]): this {
+        this.emitter().off(event, listener);
+        return this;
+    }
+
+    onPropChange<PropName extends NonFunctionPropertyNames<this>>(
+        prop: PropName,
+        listener: (value: this[PropName], prevValue: this[PropName]) => void
+    ): this {
+        const emitter = this.emitter() as TypedEmitter;
+        emitter.on(`change:${prop}`, listener);
+        return this;
+    }
+
+    offPropChange(prop: NonFunctionPropertyNames<this>, listener: () => void): this {
+        const emitter = this.emitter() as TypedEmitter;
+        emitter.off(`change:${prop}`, listener);
+        return this;
+    }
+
+    batchSet(setter: () => void, opts?: { silent?: boolean }): void {
+        const emitter = this[SymbolEmitter] as ModelTypedEmitter;
+
+        if (emitter) {
+            if (emitter.batchSet) {
+                throw new Error('Already in batchSet');
+            }
+            emitter.batchSet = true;
+            if (opts?.silent) {
+                emitter.silent = true;
+            }
+        }
+
+        try {
+            setter();
+        } finally {
+            if (emitter) {
+                emitter.batchSet = false;
+                if (opts?.silent) {
+                    emitter.silent = false;
+                }
+                if (emitter.changePending) {
+                    emitter.changePending = false;
+                    if (!opts?.silent) {
+                        emitter.emit('change');
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/framework/model.spec.ts
@@ -0,0 +1,359 @@
+import { expect } from 'chai';
+import { DefaultModelEvents, Model } from 'framework/model';
+
+describe('Model', () => {
+    interface TestModelEvents extends DefaultModelEvents {
+        'my-event': (val: string) => void;
+    }
+
+    class TestModel extends Model<TestModelEvents> {
+        str = 's';
+        num = 42;
+        strOpt?: string;
+        numOpt?: number;
+
+        emitMyEvent(val: string) {
+            this.emit('my-event', val);
+        }
+    }
+
+    it('makes a new model', () => {
+        const model = new TestModel();
+
+        expect(model).to.be.instanceof(Model);
+        expect(model.str).to.eql('s');
+        expect(model.num).to.eql(42);
+        expect(model.strOpt).to.eql(undefined);
+        expect(model.numOpt).to.eql(undefined);
+    });
+
+    it('serializes a model to JSON', () => {
+        const model = new TestModel();
+
+        expect(JSON.stringify(model)).to.eql('{"str":"s","num":42}');
+
+        model.str = 'x';
+        model.num = 1;
+        model.strOpt = 'y';
+        model.numOpt = 2;
+
+        expect(JSON.stringify(model)).to.eql('{"str":"x","num":1,"strOpt":"y","numOpt":2}');
+    });
+
+    it('sets properties', () => {
+        const model = new TestModel();
+
+        model.str = 'str';
+        model.num = 1;
+        model.strOpt = 's';
+        model.numOpt = 2;
+
+        expect(model.str).to.eql('str');
+        expect(model.num).to.eql(1);
+        expect(model.strOpt).to.eql('s');
+        expect(model.numOpt).to.eql(2);
+    });
+
+    it('generates a custom event', () => {
+        const model = new TestModel();
+
+        const eventsGenerated: string[] = [];
+        model.on('my-event', (val) => {
+            eventsGenerated.push(val);
+        });
+
+        model.emitMyEvent('x');
+
+        expect(eventsGenerated).to.eql(['x']);
+    });
+
+    it('generates an event on property change', () => {
+        const model = new TestModel();
+
+        let changeEvents = 0;
+        const changeStrEvents: { val: string; prev: string }[] = [];
+        model.on('change', () => {
+            changeEvents++;
+        });
+        model.onPropChange('str', (val, prev) => {
+            changeStrEvents.push({ val, prev });
+        });
+
+        model.str = 'val';
+
+        expect(changeEvents).to.eql(1);
+        expect(changeStrEvents).to.eql([{ val: 'val', prev: 's' }]);
+    });
+
+    it('does not generate an event if property was not changed', () => {
+        const model = new TestModel();
+        model.str = 'val';
+
+        let changeEvents = 0;
+        let changeStrEvents = 0;
+        model.on('change', () => {
+            changeEvents++;
+        });
+        model.onPropChange('str', () => {
+            changeStrEvents++;
+        });
+
+        model.str = 'val';
+
+        expect(changeEvents).to.eql(0);
+        expect(changeStrEvents).to.eql(0);
+    });
+
+    it('generates an event on property deletion', () => {
+        const model = new TestModel();
+
+        model.strOpt = 'x';
+
+        let changeEvents = 0;
+        const changeStrEvents: { val?: string; prev?: string }[] = [];
+        model.on('change', () => {
+            changeEvents++;
+        });
+        model.onPropChange('strOpt', (val, prev) => {
+            changeStrEvents.push({ val, prev });
+        });
+
+        delete model.strOpt;
+
+        expect(changeEvents).to.eql(1);
+        expect(changeStrEvents).to.eql([{ val: undefined, prev: 'x' }]);
+    });
+
+    it('does not generate an event if a property was already deleted', () => {
+        const model = new TestModel();
+
+        delete model.strOpt;
+
+        let changeEvents = 0;
+        let changeStrEvents = 0;
+        model.on('change', () => {
+            changeEvents++;
+        });
+        model.onPropChange('strOpt', () => {
+            changeStrEvents++;
+        });
+
+        delete model.strOpt;
+
+        expect(changeEvents).to.eql(0);
+        expect(changeStrEvents).to.eql(0);
+    });
+
+    it('does not generate an event if a deleted property was undefined', () => {
+        const model = new TestModel();
+
+        model.strOpt = undefined;
+
+        let changeEvents = 0;
+        let changeStrEvents = 0;
+        model.on('change', () => {
+            changeEvents++;
+        });
+        model.onPropChange('strOpt', () => {
+            changeStrEvents++;
+        });
+
+        delete model.strOpt;
+
+        expect(changeEvents).to.eql(0);
+        expect(changeStrEvents).to.eql(0);
+    });
+
+    it('unsubscribes from events', () => {
+        const model = new TestModel();
+
+        let eventsGenerated = 0;
+        const eventFired = () => {
+            eventsGenerated++;
+        };
+
+        model.on('my-event', eventFired);
+        model.on('change', eventFired);
+        model.onPropChange('num', eventFired);
+
+        model.off('my-event', eventFired);
+        model.off('change', eventFired);
+        model.offPropChange('num', eventFired);
+
+        model.num = 1;
+        model.emitMyEvent('x');
+
+        expect(eventsGenerated).to.eql(0);
+    });
+
+    it('sets properties in a batch', () => {
+        const model = new TestModel();
+
+        let changed = 0;
+        model.on('change', () => {
+            changed++;
+        });
+
+        const changedProps: string[] = [];
+        model.onPropChange('str', () => {
+            changedProps.push('str');
+        });
+        model.onPropChange('num', () => {
+            changedProps.push('num');
+        });
+        model.onPropChange('strOpt', () => {
+            changedProps.push('strOpt');
+        });
+        model.onPropChange('numOpt', () => {
+            changedProps.push('numOpt');
+        });
+
+        model.batchSet(() => {
+            model.str = 'x';
+            model.num = 1;
+            model.strOpt = 'y';
+            delete model.strOpt;
+            model.numOpt = 2;
+        });
+
+        expect(model.str).to.eql('x');
+        expect(model.num).to.eql(1);
+        expect(model.strOpt).to.eql(undefined);
+        expect(model.numOpt).to.eql(2);
+
+        expect(changed).to.eql(1);
+        expect(changedProps).to.eql(['str', 'num', 'strOpt', 'strOpt', 'numOpt']);
+    });
+
+    it('does not trigger change if there were not changes in a batch', () => {
+        const model = new TestModel();
+
+        let changed = 0;
+        let changedProps: string[] = [];
+        model.on('change', () => {
+            changed++;
+        });
+        model.onPropChange('str', () => {
+            changedProps.push('str');
+        });
+        model.onPropChange('num', () => {
+            changedProps.push('num');
+        });
+
+        model.batchSet(() => {
+            model.str = 'x';
+            model.num = 1;
+        });
+        expect(changed).to.eql(1);
+        expect(changedProps).to.eql(['str', 'num']);
+
+        changedProps = [];
+        model.batchSet(() => {
+            model.str = 'y';
+            model.num = 2;
+        });
+        expect(changed).to.eql(2);
+        expect(changedProps).to.eql(['str', 'num']);
+
+        changedProps = [];
+        model.batchSet(() => {
+            model.str = 'y';
+            model.num = 2;
+        });
+        expect(changed).to.eql(2);
+        expect(changedProps).to.eql([]);
+    });
+
+    it('sets properties silently', () => {
+        const model = new TestModel();
+
+        let changed = 0;
+
+        model.on('change', () => changed++);
+        model.onPropChange('str', () => changed++);
+        model.onPropChange('num', () => changed++);
+        model.onPropChange('strOpt', () => changed++);
+        model.onPropChange('numOpt', () => changed++);
+
+        model.batchSet(
+            () => {
+                model.str = 'x';
+                model.num = 1;
+                model.strOpt = 'y';
+                delete model.strOpt;
+                model.numOpt = 2;
+            },
+            { silent: true }
+        );
+
+        expect(changed).to.eql(0);
+    });
+
+    it('throws an error for nested batchSet', () => {
+        const model = new TestModel();
+
+        let err: Error | undefined;
+
+        model.on('change', () => {
+            expect.fail('Not expected to get here');
+        });
+
+        model.batchSet(() => {
+            try {
+                model.batchSet(() => {
+                    expect.fail('Not expected to get here');
+                });
+            } catch (e) {
+                if (e instanceof Error) {
+                    err = e;
+                }
+            }
+        });
+
+        expect(err?.message).to.eql('Already in batchSet');
+    });
+
+    it('supports derived models', () => {
+        class TestDerivedModel extends TestModel {
+            der = 'derived';
+        }
+
+        const model = new TestDerivedModel();
+
+        let changed = 0;
+        const changedProps: string[] = [];
+        model.on('change', () => changed++);
+        model.onPropChange('der', () => changedProps.push('der'));
+        model.onPropChange('str', () => changedProps.push('str'));
+
+        expect(model.der).to.eql('derived');
+        expect(model.str).to.eql('s');
+
+        model.der = 'new-der';
+        model.str = 'new-str';
+
+        expect(model.der).to.eql('new-der');
+        expect(model.str).to.eql('new-str');
+
+        expect(changed).to.eql(2);
+        expect(changedProps).to.eql(['der', 'str']);
+    });
+
+    it('supports models without specifying events', () => {
+        class TestModelWithDefaultEvents extends Model {
+            prop = 'str';
+        }
+
+        const model = new TestModelWithDefaultEvents();
+
+        let changed = 0;
+        let changedProps = 0;
+        model.on('change', () => changed++);
+        model.onPropChange('prop', () => changedProps++);
+
+        model.prop = 'changed';
+
+        expect(changed).to.eql(1);
+        expect(changedProps).to.eql(1);
+    });
+});
